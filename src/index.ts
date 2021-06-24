cba7a3e68979571c4b2140ee5e8a67496f79f898
Guilherme <guilhermet213@gmail.com>
2021-06-24 12:09:12 -0300

    feat: implement entrypoint (index.ts)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Ola mundo');
+import dotenv from 'dotenv';
+import { Bot } from './client';
+
+dotenv.config();
+
+const bot = new Bot();
+
+if (process.env.DISCORD_BOT_TOKEN) {
+  bot.start({ token: process.env.DISCORD_BOT_TOKEN });
+} else {
+  throw new Error('Invalid or missing Discord access token');
+}
